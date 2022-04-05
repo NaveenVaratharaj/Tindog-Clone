5444b8b2bee01e5e9f8771edd94af3d7127a3661
Naveen Varatharaj <88475979+NaveenVaratharaj@users.noreply.github.com>
2022-04-05 19:48:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// var tweets = window.prompt("Enter the paragraph: ");
+// var sliced = tweets.slice(141, 500);
+// alert("You have written "+ tweets.length +" characters, you have" + (140 - tweets.length) + " characters remaining.");
+// console.log(tweets)
+
+/
